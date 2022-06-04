8864f805dc8b2ba49353c93c742562bcad300ec1
Stella Maia <stellamaia97@hotmail.com>
2022-06-04 02:35:25 -0300

    Modificação EntrarView.vue

---
+++ src/views/EntrarView.vue
@@ -0,0 +1,115 @@
+<template>
+  <div class="EntrarView">
+       <div class="cad">
+            <p class="card-title">Olá_nome_do_Visitante!</p>
+          </div>
+    <b-container class="container">
+        
+      <b-row>
+        <b-col class="coluna-um" cols="12" sm="12" md="6" lg="5" xl="5">
+            
+          <div>
+            <img class="img-sair" alt="Sair" src="../assets/sair.svg" />
+          </div>
+        </b-col>
+
+        <b-col >
+         
+          <div class="coluna-dois">
+            <b-row>
+              <b-col class="botao" cols="12" sm="12" md="6" lg="4" xl="3">
+                <button class="entrar">Editar Dados</button>
+              </b-col>
+              <b-col class="botao" cols="12" sm="12" md="6" lg="4" xl="3">
+                <button class="entrar">Sair</button>
+              </b-col>
+            </b-row>
+          </div>
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+<style scoped>
+.EntrarView {
+  background-color: rgba(113, 113, 222, 0.841);
+  height: 100vh;
+
+}
+.container {
+  padding-top: 200px;
+}
+.cad{
+    padding-top: 50px;
+}
+.card-title {
+  padding-top: 30px;
+  text-align: center;
+  color: white;
+  font-size: 40px;
+}
+.img-sair {
+  height: 350px;
+  width: 400px;
+}
+.coluna-dois{
+    padding-top: 20px;
+}
+.cards {
+  background-color: rgb(255, 255, 255);
+
+  width: 100%;
+  height: 50vh;
+  border-radius: 20px;
+}
+
+.botao {
+  text-align: center;
+  padding-bottom:20px ;
+}
+
+.entrar {
+  background-color: rgba(113, 95, 205, 0.446);
+  border: none;
+  border-radius: 30px;
+  height: 50px;
+  width: 150px;
+  color: white;
+  font-size: 20px;
+  font-weight: bold;
+  cursor: pointer;
+}
+
+
+.entrar:hover {
+  background-color: rgba(63, 45, 201, 0.551);
+  transition: 1s;
+}
+
+@media screen and (min-width: 320px) and (max-width: 480px) {
+    .container {
+  padding-top: 20px;
+}
+.cad{
+    padding-top: 20px;
+}
+.card-title {
+padding-top:0;
+  text-align: center;
+  color: white;
+  font-size: 20px;
+}
+.img-sair {
+ 
+  width: 280px;
+}
+.coluna-dois{
+    padding-top: 0px;
+}
+.botao {
+
+  padding-bottom:10px ;
+}
+}
+</style>

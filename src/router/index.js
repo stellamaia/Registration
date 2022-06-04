6a8cf17394d5099024b456879bfe858f77e6c4f3
Stella Maia <stellamaia97@hotmail.com>
2022-06-04 02:35:13 -0300

    Modificação index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import EntrarView from '../views/EntrarView.vue'
 
 Vue.use(VueRouter)
 
@@ -17,7 +18,12 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+  },
+  {
+    path: '/entrar',
+    name: 'entrar',
+    component: EntrarView
+  },
 ]
 
 const router = new VueRouter({

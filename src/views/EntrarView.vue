1195154645a629f7cf35759bc1020199c5a036dc
Stella Maia <stellamaia97@hotmail.com>
2022-06-06 01:04:08 -0300

    Modificação EntarView

--- src/views/EntrarView.vue
+++ src/views/EntrarView.vue
@@ -1,20 +1,18 @@
 <template>
   <div class="EntrarView">
-       <div class="cad">
-            <p class="card-title">Olá_nome_do_Visitante!</p>
-          </div>
+    <div class="cad">
+      <p v-for="dado in dados"
+          :key="dado.id" class="card-title"> Olá {{ dado.nome }}!</p>
+    </div>
     <b-container class="container">
-        
       <b-row>
         <b-col class="coluna-um" cols="12" sm="12" md="6" lg="5" xl="5">
-            
           <div>
             <img class="img-sair" alt="Sair" src="../assets/sair.svg" />
           </div>
         </b-col>
 
-        <b-col >
-         
+        <b-col>
           <div class="coluna-dois">
             <b-row>
               <b-col class="botao" cols="12" sm="12" md="6" lg="4" xl="3">
@@ -31,17 +29,42 @@
   </div>
 </template>
 
+
+
+<script>
+import axios from 'axios'
+
+const baseURL = "http://localhost:3000/dados";
+export default{
+  name:"EntrarView",
+  data(){
+    return{
+      dados:[],
+    };
+  },
+  created(){
+    axios.get(baseURL).then((res) =>{
+      this.dados = res.data;
+      console.log(res.data)
+    });
+  }
+
+}
+</script>
+
+
+
+
 <style scoped>
 .EntrarView {
   background-color: rgba(113, 113, 222, 0.841);
   height: 100vh;
-
 }
 .container {
   padding-top: 200px;
 }
-.cad{
-    padding-top: 50px;
+.cad {
+  padding-top: 50px;
 }
 .card-title {
   padding-top: 30px;
@@ -53,8 +76,8 @@
   height: 350px;
   width: 400px;
 }
-.coluna-dois{
-    padding-top: 20px;
+.coluna-dois {
+  padding-top: 20px;
 }
 .cards {
   background-color: rgb(255, 255, 255);
@@ -66,7 +89,7 @@
 
 .botao {
   text-align: center;
-  padding-bottom:20px ;
+  padding-bottom: 20px;
 }
 
 .entrar {
@@ -81,35 +104,32 @@
   cursor: pointer;
 }
 
-
 .entrar:hover {
   background-color: rgba(63, 45, 201, 0.551);
   transition: 1s;
 }
 
 @media screen and (min-width: 320px) and (max-width: 480px) {
-    .container {
-  padding-top: 20px;
-}
-.cad{
+  .container {
     padding-top: 20px;
-}
-.card-title {
-padding-top:0;
-  text-align: center;
-  color: white;
-  font-size: 20px;
-}
-.img-sair {
- 
-  width: 280px;
-}
-.coluna-dois{
+  }
+  .cad {
+    padding-top: 20px;
+  }
+  .card-title {
+    padding-top: 0;
+    text-align: center;
+    color: white;
+    font-size: 20px;
+  }
+  .img-sair {
+    width: 280px;
+  }
+  .coluna-dois {
     padding-top: 0px;
-}
-.botao {
-
-  padding-bottom:10px ;
-}
+  }
+  .botao {
+    padding-bottom: 10px;
+  }
 }
 </style>
